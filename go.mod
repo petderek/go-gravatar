f5e5db5f027f28107eb093867457099404afefa2
Derek Petersen <derek@djp.fyi>
2021-02-20 04:27:20 -0800

    working with other flag library

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/petderek/go-gravatar
+
+go 1.13
+
+require github.com/petderek/dflag v0.1.0
--- grav/main.go
+++ grav/main.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"crypto"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 
+	"github.com/petderek/dflag"
 	. "github.com/petderek/go-gravatar"
 )
 
-var (
-	check  = flag.Bool("check", false, "queries the service for existence. returns the url only if found. exits with (2) if not found.")
-	save   = flag.String("save", "", "save the photo saves the content to the filename specified. implies check")
-	size   = flag.Int("size", 0, "the optional size in pixels")
-	domain = flag.String("domain", "", "the optional base domain to use instead of gravatar, such as http://cdn.libravatar.org")
-	sha256 = flag.Bool("sha256", false, "use sha256 instead of md5, for libravatar and friends")
-)
+var flags = struct {
+	Check  bool   `usage:"queries the service for existence. returns the url only if found. exits with (2) if not found"`
+	Save   string `usage:"save the photo saves the content to the filename specified. implies check"`
+	Size   int    `usage:"size in pixels"`
+	Domain string `usage:"base domain to use instead of gravatar, such as http://cdn.libravatar.org"`
+	SHA256 bool   `usage:"use sha256 instead of md5, for libravatar and friends"`
+}{}
 
 // gravatar's api returns a silhouette by default. passing this arg forces a 404 if an image isn't found
 const respondWith404 = "404"
 
 func main() {
-	flag.Parse()
+	_ = dflag.Parse(&flags)
 
-	if flag.NArg() <= 0 {
+	if len(dflag.Args()) <= 0 {
 		errlog(exitError, "expected first argument to be an email address.")
 	}
 
 	var location *url.URL
-	if *domain != "" {
-		u, err := url.Parse(*domain)
+	if flags.Domain != "" {
+		u, err := url.Parse(flags.Domain)
 		if err != nil {
 			errlog(exitError, "domain was invalid: ", err.Error())
 		}
@@ -40,17 +40,17 @@ func main() {
 	}
 
 	g := &Gravatar{
-		Size:           *size,
+		Size:           flags.Size,
 		DefaultPicture: respondWith404,
 		BaseDomain:     location,
 	}
 
-	if *sha256 {
+	if flags.SHA256 {
 		g.Hash = crypto.SHA256
 	}
 
-	result := g.AvatarUrl(flag.Arg(0))
-	if *check || *save != "" {
+	result := g.AvatarUrl(dflag.Arg(0))
+	if flags.Check || flags.Save != "" {
 		res, err := http.Get(result)
 		if err != nil {
 			errlog(exitError, "unable to compete http request: ", err.Error())
@@ -61,12 +61,12 @@ func main() {
 			errlog(exitNotFound, "not found")
 		}
 
-		if *save != "" {
+		if flags.Save != "" {
 			data, err := ioutil.ReadAll(res.Body)
 			if err != nil {
 				errlog(exitError, "unable to read response: ", err.Error())
 			}
-			err = ioutil.WriteFile(*save, data, os.ModePerm)
+			err = ioutil.WriteFile(flags.Save, data, os.ModePerm)
 			if err != nil {
 				errlog(exitError, "unable to write file: ", err.Error())
 			}
